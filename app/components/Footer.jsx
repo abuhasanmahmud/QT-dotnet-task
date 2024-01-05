143e7d43bafb425ac02879e5a8c57237cc8bb6a4
abuhasanmahmud <mahmud.me420@gmail.com>
2024-01-05 23:21:39 +0600

    feat(V-1) footer desktop version done

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -7,13 +7,13 @@ import { CiInstagram } from "react-icons/ci";
 const Footer = () => {
   return (
     <div
-      className="flex flex-row items-center  gap-5 pb-20 pt-5 "
+      className="sm:flex flex-row items-center  gap-5 pb-20 pt-10 "
       style={{ backgroundColor: "#F99F1C" }}
     >
-      <div className="basis-2/3">
-        <div className=" ml-32 ">
-          <div className="w-3/4 relative">
-            <input type="text" placeholder="Enter your email" className="w-full p-3 rounded-lg  " />
+      <div className="sm:basis-2/3">
+        <div className=" sm:ml-32  ">
+          <div className="sm:w-3/4 w-4/4 mx-5 relative ">
+            <input type="text" placeholder="Enter your email" className="w-full  p-3 rounded-lg  " />
             <button
               className="px-4 absolute top-2 right-2 text-white rounded-lg"
               style={{ backgroundColor: "#FC6011" }}
@@ -37,7 +37,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="basis-1/3">
+      <div className="sm:basis-1/3 hidden sm:block">
         <img className="w-60" src="/Image2.png" alt="" />
       </div>
     </div>
