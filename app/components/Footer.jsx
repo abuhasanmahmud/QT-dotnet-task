e0734a5a8c460d436b86a4432cb916a17e563873
abuhasanmahmud <mahmud.me420@gmail.com>
2024-01-05 23:49:30 +0600

    feat(V-1) footer desktop version done

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -14,25 +14,31 @@ const Footer = () => {
         <div className=" sm:ml-32  ">
           <div className="sm:w-3/4 w-4/4 mx-5 relative ">
             <input type="text" placeholder="Enter your email" className="w-full  p-3 rounded-lg  " />
-            <button
-              className="px-4 absolute top-2 right-2 text-white rounded-lg"
-              style={{ backgroundColor: "#FC6011" }}
-            >
+            <button className="px-4 absolute top-2 right-2 sm:text-white rounded-lg text-orange-600 font-bold">
               Subscribe <span className="text-xl">&#8594;</span>
             </button>
           </div>
 
           <div className=" flex flex-col-reverse justify-center items-center gap-12 sm:flex  sm:items-end sm:justify-between sm:gap-5 sm:mt-20">
             <div className="">
-              <p className="text-2xl font-extrabold ">
+              <p className="text-4xl font-extrabold  ">
                 pti<span style={{ color: "#FC6011" }}>.</span>
               </p>
-              <p>&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
+              <p className="font-bold">Copyright&copy;Tripp. All rights reserved.</p>
             </div>
             <div className="mr-14 flex gap-2">
-              <FaGoogle className="bg-gray-100 p-2 rounded-full " size={40} color="#FC6011" />
-              <FaTwitter className="bg-gray-100 p-2 rounded-full " size={40} color="#FC6011" />
-              <CiInstagram className="bg-gray-100 p-2 rounded-full " size={40} color="#FC6011" />
+              <FaGoogle
+                className="sm:bg-gray-100 sm:text-orange-600 p-2 rounded-full  bg-orange-600 text-gray-100 "
+                size={40}
+              />
+              <FaTwitter
+                className="sm:bg-gray-100 sm:text-orange-600 p-2 rounded-full  bg-orange-600 text-gray-100 "
+                size={40}
+              />
+              <CiInstagram
+                className="sm:bg-gray-100 sm:text-orange-600 p-2 rounded-full  bg-orange-600 text-gray-100 "
+                size={40}
+              />
             </div>
           </div>
         </div>
