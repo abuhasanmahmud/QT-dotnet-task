7e751f8d16c762addef2938dc28c6de89d56d42f
abuhasanmahmud <mahmud.me420@gmail.com>
2024-01-06 18:52:59 +0600

    feat(v-1) home page top done

--- app/components/HomeTop.jsx
+++ app/components/HomeTop.jsx
@@ -2,17 +2,18 @@ import React from "react";
 
 const HomeTop = () => {
   return (
-    <div className="py-10  ">
-      <div
-        className="grid sm:grid-cols-2 items-center justify-center px-10 rounded-3xl "
-        style={{ backgroundColor: "#F99F1C" }}
-      >
-        <div>
-          <p>Deliver Food To Your Door Step</p>
-          <p>Authentic Food, Quick Service, Fast Delivery</p>
+    <div className="py-10">
+      <div className="rounded-3xl md:bg-custom-yellow md:flex md:items-center ">
+        <div className="md:ml-10 md:text-white">
+          <p className="text-center text-5xl font-semibold mx-3 md:text-3xl  md:text-left ">
+            Deliver Food To Your Door Step
+          </p>
+          <p className="text-center mt-3 md:text-left md:ml-3">
+            Authentic Food, Quick Service, Fast Delivery
+          </p>
         </div>
-        <div>
-          <img src="/Image1.png" alt="" />
+        <div className="md:bg-custom-yellow bg-custom-orange rounded-3xl relative w-full h-64 mt-14 ">
+          <img src="/Image1.png" alt="" className=" absolute top-[-66px] w-full h-[20rem]" />
         </div>
       </div>
     </div>
