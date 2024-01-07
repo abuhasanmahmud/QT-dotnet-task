d56d4365a2104e5bc626cff014bcbbe75f13b1e6
abuhasanmahmud <mahmud.me420@gmail.com>
2024-01-07 17:32:23 +0600

    feat(V-1) popular

--- app/components/Popular.jsx
+++ app/components/Popular.jsx
@@ -81,7 +81,7 @@ const Popular = () => {
                   <div className="">
                     {/* <img src={i?.ImageUrl} alt="" className="w-60 h-40 " /> */}
 
-                    <Image src={i?.ImageUrl} width={300} height={100} />
+                    <Image src={i?.ImageUrl} className="w-60 h-40 " width={300} height={100} />
                     <p className="my-2 text-center">{i?.Name}</p>
                   </div>
                 </SwiperSlide>
